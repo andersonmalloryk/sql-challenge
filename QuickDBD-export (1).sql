7e187d964a8d91a057f2f0bbfcaaa620334bfede
mallory anderson <mallorykanderson@mallorys-Air.T-mobile.com>
2021-03-04 20:52:44 -0600

    attempt at setting up tables, imports did not work

---
+++ QuickDBD-export (1).sql
@@ -0,0 +1,77 @@
+﻿-- create table for departments data
+CREATE TABLE departments (
+    dept_no VARCHAR   NOT NULL,
+    dept_name VARCHAR   NOT NULL,
+    CONSTRAINT pk_departments PRIMARY KEY (
+        dept_no
+     )
+);
+-- create table for department employee data
+-- encountered error when uploading files because of duplicate data 
+CREATE TABLE dept_employee (
+    employee_no INT   NOT NULL,
+    dept_no VARCHAR   NOT NULL,
+	CONSTRAINT pk_dept_employee PRIMARY KEY (
+		employee_no
+	)
+);
+-- create table for department manager data
+CREATE TABLE dept_manager (
+    dept_no INT   NOT NULL,
+    employee_no INT   NOT NULL,
+    CONSTRAINT pk_dept_manager PRIMARY KEY (
+        dept_no
+     )
+);
+-- create table for employee data
+CREATE TABLE employees (
+    employee_no INT   NOT NULL,
+    emp_title_id VARCHAR   NOT NULL,
+    birth_date VARCHAR   NOT NULL,
+    first_name VARCHAR   NOT NULL,
+    last_name VARCHAR   NOT NULL,
+    sex VARCHAR   NOT NULL,
+    hire_date VARCHAR   NOT NULL,
+    CONSTRAINT pk_employees PRIMARY KEY (
+        employee_no
+     )
+);
+-- create table for salary data
+CREATE TABLE salaries (
+    employee_no INT   NOT NULL,
+    salary INT   NOT NULL,
+	CONSTRAINT pk_salaries PRIMARY KEY (
+		employee_no	
+	)
+);
+-- create table for title data
+CREATE TABLE titles (
+    title_id VARCHAR   NOT NULL,
+    title VARCHAR   NOT NULL,
+    CONSTRAINT pk_titles PRIMARY KEY (
+        title_id
+     )
+);
+
+ALTER TABLE dept_employee ADD CONSTRAINT fk_dept_employee_employee_no FOREIGN KEY(employee_no)
+REFERENCES employees (employee_no);
+
+ALTER TABLE dept_employee ADD CONSTRAINT fk_dept_employee_dept_no FOREIGN KEY(dept_no)
+REFERENCES departments (dept_no);
+
+ALTER TABLE dept_manager ADD CONSTRAINT fk_dept_manager_employee_no FOREIGN KEY(employee_no)
+REFERENCES employees (employee_no);
+
+ALTER TABLE employees ADD CONSTRAINT fk_employees_emp_title_id FOREIGN KEY(emp_title_id)
+REFERENCES titles (title_id);
+
+ALTER TABLE salaries ADD CONSTRAINT fk_salaries_employee_no FOREIGN KEY(employee_no)
+REFERENCES employees (employee_no);
+
+-- check the tables 
+SELECT * FROM departments;
+SELECT * FROM dept_employee;
+SELECT * FROM dept_manager;
+SELECT * FROM employees;
+SELECT * FROM salaries;
+SELECT * FROM titles;
